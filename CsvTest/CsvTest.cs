f00601417c4fcea340f07fe28b0305e0fc8ff378
Kimserey Lam <kimserey.lam@gmail.com>
2016-11-15 17:56:55 +0000

    read csv from xamarin android

---
+++ CsvTest/CsvTest.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using CsvHelper;
+using Xamarin.Forms;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CsvTest
+{
+	public interface IFileService {
+
+		StreamReader GetFileStream(string name);
+	}
+
+	public class Record
+	{
+		public string Title { get; set; }
+		public decimal Amount { get; set; }
+		public DateTime Date { get; set; }
+	}
+
+	public class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			var csvName = "test.csv";
+			var button = new Button { Text = "Try read CSV" };
+
+			button.Clicked += async (sender, e) => {
+				var list = new List<Record>();
+				using (var reader = DependencyService.Get<IFileService>().GetFileStream(csvName))
+				{
+					if (reader != null)
+					{
+						using (var csv = new CsvReader(reader))
+						{
+							while (csv.Read())
+							{
+								list.Add(new Record
+								{
+									Date = csv.GetField<DateTime>(0),
+									Title = csv.GetField<string>(1),
+									Amount = csv.GetField<decimal>(2)
+								});
+							}
+						}
+					}
+				}
+				await this.DisplayAlert("Test", "worked", "OK");
+			};
+
+			Title = "Csv reader test";
+			Content = new StackLayout
+			{
+				Children = {
+					button
+				}
+			};
+		}
+	}
+
+	public class App : Application
+	{
+		public App()
+		{
+			MainPage = new NavigationPage(new MainPage());
+		}
+	}
+}
---
+++ Droid/MainActivity.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(CsvTest.Droid.FileService))]
+namespace CsvTest.Droid
+{
+	public class FileService : IFileService
+	{
+		// Returns null if file does not exists.
+		public StreamReader GetFileStream(string name)
+		{
+			var path = Path.Combine(Android.OS.Environment.ExternalStorageDirectory.AbsolutePath, "CsvTest", "test.csv");
+			if(File.Exists(path))
+				return new StreamReader(File.OpenRead(path));
+			else 
+				return null;
+		}
+	}
+
+	[Activity(Label = "CsvTest.Droid", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+	{
+		protected override void OnCreate(Bundle bundle)
+		{
+			TabLayoutResource = Resource.Layout.Tabbar;
+			ToolbarResource = Resource.Layout.Toolbar;
+
+			base.OnCreate(bundle);
+
+			global::Xamarin.Forms.Forms.Init(this, bundle);
+
+			LoadApplication(new App());
+		}
+	}
+}
